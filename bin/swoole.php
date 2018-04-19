063225fa16136f6d3b2b9d5e99325dfb9bfa8780
xmlscript <xmlscript@gmail.com>
2018-04-19 10:27:50 +0800

    dev://

--- bin/swoole.php
+++ bin/swoole.php
@@ -2,9 +2,9 @@
 <?php
 PHP_SAPI==='cli' or die(!ob_end_clean());
 
-require 'vendor/autoload.php';
+require 'Zend/autoload.php';//FIXME 如果把api的子类也做成库，就不需要反复require了。而且项目update时，并不会下载库里的vendor
 
-$http = new Swoole\Http\Server('127.0.0.1', 9501);
+$http = new Swoole\Http\Server('127.0.0.1', 9501);//FIXME 允许命令行参数设置！！！
 
 $http->on('request', function ($request, $response){
 
@@ -14,7 +14,7 @@ $http->on('request', function ($request, $response){
   //把Swoole非标准的请求头导入PHP标准的$_SERVER变量，一定要在结束时把这些新增的key删除！！！
   foreach($request->header as $k=>$v) $_SERVER['HTTP_'.strtoupper(str_replace('-','_',$k))] = $v;
   
-  $obj = new xmlscript\ping;//根据路由不同，new的实际类也不同
+  $obj = new xmlscript\ping;//FIXME 这里不能硬编码！！！
 
   $str = $obj();
 
